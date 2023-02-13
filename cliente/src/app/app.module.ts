769fa43075509d139b29c858248c2966c6a8bd11
juan75amieva <juan75amieva@gmail.com>
2023-02-13 07:40:35 +0100

    feat:creating client

---
+++ cliente/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CrearProductoComponent,
+    ListarProductosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
