8015545d011c1aa65e28c09e4560dc66e5084b40
juan75amieva <juan75amieva@gmail.com>
2023-02-14 11:30:13 +0100

    feat: toast add

--- cliente/src/app/components/crear-producto/crear-producto.component.ts
+++ cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -9,20 +12,32 @@ import {FormBuilder, FormGroup, Validators } from '@angular/forms'
 export class CrearProductoComponent implements OnInit {
   productoForm: FormGroup
 
-  constructor(private fb: FormBuilder){
+  constructor(private fb: FormBuilder,
+    private router: Router,
+    private toastr: ToastrService) {
     this.productoForm = this.fb.group({
-      producto: ['',Validators.required],
-      categoria: ['',Validators.required],
-      ubicacion: ['',Validators.required],
-      precio: ['',Validators.required]
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required]
     })
-   }
-  
-  ngOnInit(): void{
+  }
+
+  ngOnInit(): void {
 
   }
 
-  agregarProducto(){
-    console.log(this.productoForm)
+  agregarProducto() {
+
+
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+    }
+    console.log(PRODUCTO)
+    this.toastr.success('El producto fue registrado con exito ', 'Producto registrado!');
+    this.router.navigate(['/'])
   }
 }
